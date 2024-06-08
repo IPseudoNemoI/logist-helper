884266bcdd2dc2fa0503862c932beed6ae7f8e1a
IPseudoNemoI <nemoarkoni@mail.ru>
2024-06-08 18:03:27 +0700

    Prepare project

--- app/src/main/java/dev/pseudo/logisthelper/presentation/main/MainActivity.kt
+++ app/src/main/java/dev/pseudo/logisthelper/presentation/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package dev.pseudo.logisthelper
+package dev.pseudo.logisthelper.presentation.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import dev.pseudo.logisthelper.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
